d83b385d7714ff2ffdc4f93ae995670ed38d808e
Robin Kelsick II <robinkelsickii@gmail.com>
2021-09-28 19:26:30 -0400

    need to move 3 icons in top right

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 var time = document.getElementById('time'); 
+
+function currentTime(){
+    let clock = new Date;
+
+    let hours = clock.getHours();
+    let minutes = clock.getMinutes(); 
+
+    time.innerHTML = `${hours} : ${minutes}`
+}
+
+function doubleDigits(time) {
+    if (time < 10) {
+        return '0' + time;
+    }
+    return time;
+}
+
+setInterval(currentTime, 1000); 
